c4b205246175c3e9bb14b8768347bf912f7fc1e1
Simeon Kolchin <simeon.kolchin@icloud.com>
2024-07-13 17:37:47 +0500

    final

--- gameapi.py
+++ gameapi.py
@@ -20,22 +20,20 @@ class GameAPI:
             logging.info("DEBUG mode: Skipping registration for round.")
         else:
 
-            while True:
-                url = f"{self.base_url}/participate"
-                response = requests.put(url, headers=self.headers)
-                print(response.status_code, response.text)
-                if response.status_code == 200:
-                    logging.info("Registered for round successfully!")
-                    return
-                else:
-                    logging.error(f"Failed to register for round: {response.status_code}, {response.text}")
-                    time.sleep(0.5)
+            url = f"{self.base_url}/participate"
+            response = requests.put(url, headers=self.headers)
+            
+            print(response.status_code, response.text)
+            if response.status_code == 200:
+                logging.info("Registered for round successfully!")
+            else:
+                logging.error(f"Failed to register for round: {response.status_code}, {response.text}")
 
     def get_game_state(self):
         if self.debug:
             logging.info("DEBUG mode: Using local game state.")
 
-            with open(f'games/10_26_{min(self.hod_num, 30)}.json', 'r') as f:
+            with open(f'games/15_25_{min(self.hod_num, 112)}.json', 'r') as f:
                 self.hod_num += 1
                 game_state = json.load(f)
                 return game_state
@@ -45,7 +43,22 @@ class GameAPI:
             if response.status_code == 200:
                 return response.json()
             else:
+                print(response.text)
                 logging.error(f"Failed to get game state: {response.status_code}, {response.text}")
+                return None
+            
+    def get_world_info(self):
+        if self.debug:
+            return {
+                "zpots": []
+            }
+        else:
+            url = f"{self.base_url}/world"
+            response = requests.get(url, headers=self.headers)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                logging.error(f"Failed to get world info: {response.status_code}, {response.text}")
                 return None
 
     def send_commands(self, command):
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from datetime import datetime
+import math
 
 logging.basicConfig(filename='game_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
 
@@ -27,25 +28,45 @@ def log_and_save_game_state(game_state, filename="game_state.json"):
         json.dump(game_state, f, indent=4)
     logging.info(f"Game state saved to {filename}")
 
-def build_map(game_state, map_filename="game_map.txt"):
+def build_map(game_state, shots, block_shots, buildings, map_filename="game_map.txt"):
     map_data = {}
     if "base" in game_state and game_state['base'] is not None:
         for block in game_state["base"]:
             map_data[(block["x"], block["y"])] = 'B'  # B - Base block
     if "zombies" in game_state and game_state['zombies'] is not None:
         for zombie in game_state["zombies"]:
-            # map_data[(zombie["x"], zombie["y"])] = 'Z'  # Z - Zombie
             map_data[(zombie["x"], zombie["y"])] = f'Z{zombie["health"]}'  # Z{health} - Zombie with health
+
+    if "enemyBlocks" in game_state and game_state["enemyBlocks"] is not None:
+        for block in game_state["enemyBlocks"]:
+            if block.get("isHead"):
+                map_data[(block["x"], block["y"])] = 'enemy_head'  # E - Enemy head block
+            else:
+                map_data[(block["x"], block["y"])] = 'enemy_base'  # b - Enemy base block
+
+    try:
+        if 'world' in game_state:
+            for x in game_state['world']:
+                if x['type'] == 'wall':
+                    map_data[(x['x'], x['y'])] = "wall"
+                elif x['type'] == 'default':
+                    map_data[(x['x'], x['y'])] = 'spot'
+    except:
+        print('Ошибка в build_map создание стен')
 
     if not map_data:
         return
 
-    # Определение границ карты
-    all_coords = map_data.keys()
-    min_x = min(coord[0] for coord in all_coords)
-    max_x = max(coord[0] for coord in all_coords)
-    min_y = min(coord[1] for coord in all_coords)
-    max_y = max(coord[1] for coord in all_coords)
+    if not map_data:
+        # Если нет данных для отображения, установим границы по умолчанию
+        min_x, max_x, min_y, max_y = 0, 10, 0, 10
+    else:
+        # Определение границ карты
+        all_coords = map_data.keys()
+        min_x = min(coord[0] for coord in all_coords)
+        max_x = max(coord[0] for coord in all_coords)
+        min_y = min(coord[1] for coord in all_coords)
+        max_y = max(coord[1] for coord in all_coords)
     
     # Построение карты
     with open(map_filename, 'w') as f:
@@ -60,42 +81,85 @@ def build_map(game_state, map_filename="game_map.txt"):
     logging.info(f"Game map saved to {map_filename}")
     
     # Визуализация карты
-    visualize_map(map_data, min_x, max_x, min_y, max_y)
+    visualize_map(game_state, map_data, min_x, max_x, min_y, max_y, shots, block_shots, buildings)
 
-def visualize_map(map_data, min_x, max_x, min_y, max_y):
+def visualize_map(game_state, map_data, min_x, max_x, min_y, max_y, shots, block_shots, buildings):
     fig, ax = plt.subplots()
     for (x, y), value in map_data.items():
-        if value == 'B':
+        if value == 'H':
+            color = 'blue'  # Head block (Control center)
+        elif value == 'B':
             color = 'green'
+        elif value == 'enemy_head':
+            color = 'purple'
+        elif value == 'enemy_base':
+            color = 'pink'
         elif value.startswith('Z'):
             color = 'red'
             zombie_hp = value[1:]
             ax.text(x + 0.5, y + 0.5, zombie_hp, color='white', ha='center', va='center')
+        elif value == 'wall':
+            color = 'gray'
+        elif value == 'spot':
+            color = 'orange'
         else:
             color = 'white'
         rect = patches.Rectangle((x, y), 1, 1, linewidth=1, edgecolor='black', facecolor=color)
         ax.add_patch(rect)
-    
+
+    for start, end in shots:
+        ax.arrow(start[0] + 0.5, start[1] + 0.5, end[0] - start[0], end[1] - start[1], head_width=0.1, head_length=0.3, fc='grey', ec='grey')
+    for start, end in block_shots:
+        ax.arrow(start[0] + 0.5, start[1] + 0.5, end[0] - start[0], end[1] - start[1], head_width=0.1, head_length=0.3, fc='black', ec='black')
+    for x, y in buildings:
+        ax.plot(x + 0.5, y + 0.5, marker='^', color='black', markersize=12)
+
+    player_info = game_state["player"]
+    player_text = f"Name: {player_info['name']} Gold: {player_info['gold']} Points: {player_info['points']} Zombie Kills: {player_info['zombieKills']} Enemy Block Kills: {player_info['enemyBlockKills']}"
+    plt.text(min_x - 1, max_y + 2, player_text, fontsize=5, bbox=dict(facecolor='white', alpha=0.5))
+
     plt.xlim(min_x - 1, max_x + 1)
     plt.ylim(min_y - 1, max_y + 1)
     plt.gca().set_aspect('equal', adjustable='box')
     plt.savefig("game_map_visualized.png")
     logging.info("Game map visualized and saved to game_map_visualized.png")
 
-def find_build_coords(base_blocks, map_data):
+def euclidean_distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def minimum_distance(main_point, other_points):
+    x0, y0 = main_point
+    distances = [euclidean_distance(x0, y0, x, y) for x, y in other_points]
+    return min(distances)
+
+def find_build_coords(base_blocks, map_data, game_state):
     """Ищет доступные координаты для строительства новых клеток рядом с существующими блоками базы."""
     new_coords = []
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    zombie_spots = []
+    if 'world' in game_state and game_state['world'] is not None:
+        for x in game_state['world']:
+            if x['type'] == 'default':
+                zombie_spots.append((x['x'], x['y']))
     
     for block in base_blocks:
         for dx, dy in directions:
             new_coord = (block["x"] + dx, block["y"] + dy)
+            
             if new_coord not in map_data:
-                new_coords.append(new_coord)
-    
+                if len(zombie_spots):
+                    new_coords.append((new_coord[0], new_coord[1], minimum_distance(new_coord, zombie_spots)))
+                else:
+                    new_coords.append(new_coord)
+
+    if len(zombie_spots):
+        new_coords.sort(key=lambda x: x[2], reverse=True)
+
     return new_coords
 
 def attack_enemy_bases(game_state, command):
+    shots = []
     # Атакуем чужие базы
     if "base" in game_state and game_state['base'] is not None and "enemyBlocks" in game_state and game_state['enemyBlocks'] is not None:
         base_blocks = game_state["base"]
@@ -106,107 +170,74 @@ def attack_enemy_bases(game_state, command):
             x, y = block["x"], block["y"]
             attack_radius = block["range"]
 
-            for enemy_block in enemy_blocks:
-                ex, ey = enemy_block["x"], enemy_block["y"]
+            for i in range(len(enemy_blocks)):
+                ex, ey = enemy_blocks[i]["x"], enemy_blocks[i]["y"]
                 distance = ((x - ex) ** 2 + (y - ey) ** 2) ** 0.5
-                if distance <= attack_radius:
+                if distance <= attack_radius and enemy_blocks[i]['health'] > 0:
                     print(f'ATTACK BASE: {block_id} - {ex} - {ey}')
                     command.add_attack(block_id, ex, ey)
+                    enemy_blocks[i]['health'] -= block['attack']
+                    shots.append(((x,y),(ex,ey)))
+                    break
+    return shots
 
 
-def filter_zombies_by_direction(zombies, base_blocks, turns=10):
-    potential_zombies = []
-    other_zombies = []
+# def filter_zombies_by_direction(zombies, base_blocks, turns=10):
+#     potential_zombies = []
+#     other_zombies = []
 
-    for zombie in zombies:
-        zx, zy = zombie["x"], zombie["y"]
-        direction = zombie["direction"]
+#     for zombie in zombies:
+#         zx, zy = zombie["x"], zombie["y"]
+#         direction = zombie["direction"]
         
-        if zombie['type'] == 'chaos_knight':
-            potential_zombies.append(zombie)
-            continue
+#         if zombie['type'] == 'chaos_knight':
+#             potential_zombies.append(zombie)
+#             continue
 
-        path = [(zx, zy)]
-        current_x, current_y = zx, zy
+#         path = [(zx, zy)]
+#         current_x, current_y = zx, zy
 
-        for _ in range(20):
-            if direction == "up":
-                current_y -= 1
-            elif direction == "down":
-                current_y += 1
-            elif direction == "left":
-                current_x -= 1
-            elif direction == "right":
-                current_x += 1
-            path.append((current_x, current_y))
+#         for _ in range(20):
+#             if direction == "up":
+#                 current_y -= 1
+#             elif direction == "down":
+#                 current_y += 1
+#             elif direction == "left":
+#                 current_x -= 1
+#             elif direction == "right":
+#                 current_x += 1
+#             path.append((current_x, current_y))
 
-        # Проверить, попадает ли зомби на блок базы
-        for bx, by in [(block["x"], block["y"]) for block in base_blocks]:
-            if (bx, by) in path:
-                potential_zombies.append(zombie)
-                continue
-        other_zombies.append(zombie)
+#         # Проверить, попадает ли зомби на блок базы
+#         for bx, by in [(block["x"], block["y"]) for block in base_blocks]:
+#             if (bx, by) in path:
+#                 potential_zombies.append(zombie)
+#                 continue
+#         other_zombies.append(zombie)
         
 
-    return potential_zombies, other_zombies
+#     return potential_zombies, other_zombies
 
 
 def attack_zombies(game_state, command):
-    # Здесь выбираем зомби, по которым будем стрелять
-    if "base" in game_state and game_state["base"] is not None and "zombies" in game_state and game_state['zombies'] is not None:
-        base_blocks = game_state["base"]
-        zombies = game_state["zombies"]
+    zombies = [[z['x'], z['y'], z['health']] for z in game_state['zombies']]
+    shots = []
+    for block in game_state['base']:
+        block_id = block["id"]
+        x, y = block["x"], block["y"]
+        attack_radius = block["range"]
+        
+        for i in range(len(zombies)):
+            zx, zy, zhp = zombies[i]
+            distance = ((x - zx) ** 2 + (y - zy) ** 2) ** 0.5
+            if distance <= attack_radius and zhp > 0:
+                command.add_attack(block_id, zx, zy)
+                shots.append(((x, y), (zx, zy)))
+                print('attack - ', zombies[i][2], block['attack'])
+                zombies[i][2] -= block['attack']
+                break
+    return shots
 
-        filtered_zombies, other_zombies = filter_zombies_by_direction(zombies, base_blocks)
-        zombie_list = [{"x": z["x"], "y": z["y"], "hp": z["health"], "id": z["id"], "type": z["type"], "speed": z["speed"]} for z in filtered_zombies]
-        other_list = [{"x": z["x"], "y": z["y"], "hp": z["health"], "id": z["id"], "type": z["type"], "speed": z["speed"]} for z in other_zombies]
-
-        for block in base_blocks:
-            block_id = block["id"]
-            x, y = block["x"], block["y"]
-            attack_radius = block["range"]
-            attack_power = block["attack"]
-
-            nearest_zombie_idx = None
-            nearest_distance = float('inf')
-
-            for idx, zombie in enumerate(zombie_list):
-                zx, zy = zombie["x"], zombie["y"]
-                distance = ((x - zx) ** 2 + (y - zy) ** 2) ** 0.5
-                norm_distance = distance
-
-                if zombie['type'] in ['liner']:
-                    norm_distance -= 1
-                elif zombie['type'] in ['bomber', 'juggernaut', 'chaos_knight']:
-                    norm_distance -= 2
-
-                if distance <= attack_radius and norm_distance < nearest_distance and zombie['hp'] > 0:
-                    nearest_zombie_idx = idx
-                    nearest_distance = norm_distance
-
-            if nearest_zombie_idx:
-                command.add_attack(block_id, zombie_list[nearest_zombie_idx]["x"], zombie_list[nearest_zombie_idx]["y"])
-                print(f'ATTACK: {zombie_list[nearest_zombie_idx]["x"]} - {zombie_list[nearest_zombie_idx]["y"]}')
-                zombie_list[nearest_zombie_idx]["hp"] -= attack_power
-            else:
-                for idx, zombie in enumerate(other_list):
-                    zx, zy = zombie["x"], zombie["y"]
-                    distance = ((x - zx) ** 2 + (y - zy) ** 2) ** 0.5
-                    norm_distance = distance
-
-                    if zombie['type'] in ['liner']:
-                        norm_distance -= 1
-                    elif zombie['type'] in ['bomber', 'juggernaut', 'chaos_knight']:
-                        norm_distance -= 2
-
-                    if distance <= attack_radius and norm_distance < nearest_distance and zombie['hp'] > 0:
-                        nearest_zombie_idx = idx
-                        nearest_distance = norm_distance
-
-                if nearest_zombie_idx:
-                    command.add_attack(block_id, other_list[nearest_zombie_idx]["x"], other_list[nearest_zombie_idx]["y"])
-                    print(f'ATTACK: {other_list[nearest_zombie_idx]["x"]} - {other_list[nearest_zombie_idx]["y"]}')
-                    other_list[nearest_zombie_idx]["hp"] -= attack_power
 
 def handle_zombie_attack(zombie, base_blocks, map_data):
     zombie_type = zombie["type"]
@@ -234,25 +265,10 @@ def handle_zombie_attack(zombie, base_blocks, map_data):
     
     return updated_base_blocks
 
-
-# def get_connected_blocks(base_blocks):
-#     connected_blocks = set()
-#     to_visit = [block for block in base_blocks if block.get("isHead")]
-#     while to_visit:
-#         block = to_visit.pop()
-#         connected_blocks.add((block["x"], block["y"]))
-#         neighbors = [(block["x"] + dx, block["y"] + dy) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
-#         for nx, ny in neighbors:
-#             if (nx, ny) not in connected_blocks:
-#                 for b in base_blocks:
-#                     if b["x"] == nx and b["y"] == ny:
-#                         to_visit.append(b)
-#                         break
-#     return connected_blocks
-
-
 def strategy(game_state):
     command = Command()
+
+    shots, block_shots, buildings = [], [], []
 
     print("START")
 
@@ -263,50 +279,64 @@ def strategy(game_state):
         for zombie in game_state["zombies"]:
             map_data[(zombie["x"], zombie["y"])] = 'Z'
 
-        attack_zombies(game_state, command)
-        attack_enemy_bases(game_state, command)
+        if 'world' in game_state:
+            for x in game_state['world']:
+                if x['type'] == 'wall':
+                    map_data[(x['x'], x['y'])] = "wall"
+                elif x['type'] == 'default':
+                    map_data[(x['x'], x['y'])] = 'spot'
+
+        shots = attack_zombies(game_state, command)
+        block_shots = attack_enemy_bases(game_state, command)
 
         for zombie in game_state["zombies"]:
             base_blocks = handle_zombie_attack(zombie, base_blocks, map_data)
 
+
         # TODO: вот это прочекать, норм / не норм
         if game_state["player"]["gold"] > 0:
-            build_coords = find_build_coords(base_blocks, map_data)
+            build_coords = find_build_coords(base_blocks, map_data, game_state)
             # connected_blocks = get_connected_blocks(base_blocks)
             for coord in build_coords[:game_state["player"]["gold"]]:  # Ограничиваем количество новых блоков количеством золота
                 # if coord in connected_blocks:
                     command.add_build(coord[0], coord[1])
                     print(f'BUILD: {coord[0]} - {coord[1]}')
-
-            # walls проверить, пока что хз
-            # # Парсим стены и проверяем, чтобы новые блоки не строились на стенах
-            # for wall in game_state["walls"] if game_state["walls"] is not None else []:
-            #     wall_coords = (wall["x"], wall["y"])
-            #     if wall_coords in build_coords:
-            #         build_coords.remove(wall_coords)
+                    buildings.append((coord[0], coord[1]))
 
         if base_blocks:
-            # TODO: нужно находить блоки, где близко находится джегернаут или другой сильный зомби, которого нужно убить и туда поставить базу
+            
+            if 'enemyBlocks' in game_state and game_state['enemyBlocks'] is not None:
+                enemy_blocks = [(block['x'], block['y']) for block in game_state['enemyBlocks']]
+                my_blocks = [(block['x'], block['y']) for block in base_blocks]
 
-            # Здесь нужно как то выбирать безопасные клетки, куда стоит установит базу
-            # for block in base_blocks:
-            #     for zombie in game_state["zombies"] if game_state["zombies"] is not None else []:
-            #         if zombie["type"] in ["juggernaut", "chaos_knight"]:
-            #             zx, zy = zombie["x"], zombie["y"]
-            #             distance = ((block["x"] - zx) ** 2 + (block["y"] - zy) ** 2) ** 0.5
-            #             if distance <= block["range"]:
-            #                 command.add_attack(block["id"], zx, zy)
+                for my_block in my_blocks:
+                    safe = True
+                    for enemy_block in enemy_blocks:
+                        distance = ((my_block[0] - enemy_block[0]) ** 2 + (my_block[1] - enemy_block[1]) ** 2) ** 0.5
+                        if distance < 6:
+                            safe = False
+                            break
+                    if safe:
+                        command.set_move_base(my_block[0], my_block[1])
+                        break
+            else:
+                command.set_move_base(base_blocks[0]["x"], base_blocks[0]["y"])
 
-            command.set_move_base(base_blocks[0]["x"], base_blocks[0]["y"])
+    return command, shots, block_shots, buildings
 
-    return command
+def get_walls(world):
+    if world is None:
+        return
+
+    walls = [(x['x'], x['y']) for x in world if x['type'] == 'wall']
+    print('WALLS:', walls)
 
 def main():
     game_api = GameAPI(BASE_URL, TOKEN, DEBUG)
 
     while True:
 
-        # game_api.register_for_round()
+        game_api.register_for_round()
 
         cur_time = datetime.now()
         hour = cur_time.hour
@@ -315,6 +345,12 @@ def main():
         
         while True:
             game_state = game_api.get_game_state()
+
+            if not DEBUG:
+                world = game_api.get_world_info()
+                if world is not None and 'zpots' in world:
+                    game_state['world'] = world['zpots']
+
             if game_state is None:
                 break
             
@@ -324,11 +360,16 @@ def main():
                 tmp_num += 1
 
             log_and_save_game_state(game_state)
-            build_map(game_state)
 
-            command = strategy(game_state)
+            command, shots, block_shots, buildings = strategy(game_state)
             game_api.send_commands(command)
-            time.sleep(0.5)
+
+            try:
+                build_map(game_state, shots, block_shots, buildings)
+            except:
+                print('Ошибка в build_map')
+
+            time.sleep(0.25)
 
 if __name__ == "__main__":
     main()
